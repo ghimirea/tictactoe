6b78a9e1c67fe1480efd5dbc9a3f2025466a9db6
Ashim Ghimire <aghimire85@gmail.com>
2020-09-06 15:55:01 -0500

    Created 9 squares passing in the values and onClick functionality

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Board from './components/Board'
-import Game from './components/Game'
-import Square from './components/Square'
+import Game from './components/Game';
 
-function App() {
-  return (
-    <div >
-      <Board />
-      <Game />
-      <Square />
-    </div>
-  );
-}
+const App = () => <Game />;
 
 export default App;
--- tictactoe/src/components/Game.js
+++ tictactoe/src/components/Game.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import Board from './Board';
 
-const Game = () => <div>Game</div>
+const Game = () => {
+  return (
+    
+      <Board />
+    
+  );
+};
 
 export default Game;
